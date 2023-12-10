ce22a8a2571a5da286e3ec8f3a8815c5258c6018
Alp Guneysel <alp.guneysel@gmail.com>
2023-12-10 18:51:36 -0500

    day3 - add regex

--- 2023/day3/part1/src/main.rs
+++ 2023/day3/part1/src/main.rs
@@ -2,13 +2,31 @@ use std::env;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use regex::Regex;
+use std::fmt;
+
+
+
+#[derive(Debug)]
+struct NumberPosition {
+    number: i32,
+    start: usize,
+    end: usize,
+}
+
+impl fmt::Display for NumberPosition {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{{number: {}, start: {}, end: {}}}", self.number, self.start, self.end)
+    }
+}
+
 
 fn main() -> io::Result<()> {
 
     let current_dir = env::current_dir()?;
     println!("Current directory: {:?}", current_dir);
 
-    let path = Path::new("../../data/input.txt");
+    let path = Path::new("../input2.txt");
 
     // Check if the file exists
     if !path.exists() {
@@ -24,6 +42,7 @@ fn main() -> io::Result<()> {
         let line = line?;
         lines.push(line);
     }
+    //println!("lines: {:?}", lines);
 
     let lines_str: Vec<&str> = lines.iter().map(AsRef::as_ref).collect();
 
@@ -38,8 +57,34 @@ fn sum_part_numbers(schematic: &[&str]) -> i32 {
     let rows = schematic.len();
     let cols = schematic[0].len(); // assuming all rows are of equal length
 
+    println!("rows:cols {:?} : {:?}", rows,cols);
+
+
+
+    let re = Regex::new(r"\d+").unwrap();
+    let numbers = re.find_iter(schematic[0])
+                    .map(|mat| NumberPosition {
+                        number: mat.as_str().parse::<i32>().unwrap(),
+                        start: mat.start(),
+                        end: mat.end() - 1, // Adjust to make end position inclusive
+                    })
+                    .collect::<Vec<_>>();
+
+    // Print results
+    for num in numbers {
+        println!("{}", num);
+    }
+
+
+
+
+
     for (i, row) in schematic.iter().enumerate() {
+        println!("i row {:?} {:?}",i,row);
+
         for (j, &ch) in row.as_bytes().iter().enumerate() {
+            //println!("ch {:?}",ch);
+
             if ch.is_ascii_digit() {
                 // Check adjacent characters for symbols
                 if is_adjacent_to_symbol(schematic, i, j, rows, cols) {
@@ -56,6 +101,8 @@ fn sum_part_numbers(schematic: &[&str]) -> i32 {
 fn is_adjacent_to_symbol(schematic: &[&str], i: usize, j: usize, rows: usize, cols: usize) -> bool {
     let dx = [-1, 0, 1, -1, 1, -1, 0, 1];
     let dy = [-1, -1, -1, 0, 0, 1, 1, 1];
+    //println!("schematic,i,j,rows,cols {:?} : {:?}: {:?}: {:?}: {:?}",schematic,i,j,rows,cols);
+    //eprintln!("====================================================================================");
 
     for k in 0..8 {
         let new_i = i as i32 + dx[k];
